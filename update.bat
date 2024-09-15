7b31108c9b93180e2645f7ba5939f4e0d1c855d0
喵 <miaomiao>
2024-09-15 21:08:29 +0800

    修改了批处理内容

--- update.bat
+++ update.bat
@@ -1,4 +1,7 @@
 @echo off
+
+set /p update=请输入本次更新内容，按回车键确认： 
+
 echo 正在提交
-git add . & git commit -m update & git push -u origin main
+git add . & git commit -m %update% & git push -u origin main
 timeout /t 10
