48595d409b13695bcab823ae47be4f25d81a8e54
喵 <miao>
2024-09-15 21:10:43 +0800

    修改了批处理内容

--- update.bat
+++ update.bat
@@ -2,6 +2,9 @@
 
 set /p update=请输入本次更新内容，按回车键确认： 
 
+git config --global user.name "喵"
+git config --global user.email "miao"
+
 echo 正在提交
 git add . & git commit -m "%update%" & git push -u origin main
 timeout /t 10
